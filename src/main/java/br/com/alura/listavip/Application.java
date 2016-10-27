e9d3a55d660239081a16266d2efb972129870a53
wellington.siqueira <wellingtonex@gmail.com>
2016-10-26 23:12:09 -0300

    criando ConvidadoController e index.html

--- src/main/java/br/com/alura/listavip/Application.java
+++ src/main/java/br/com/alura/listavip/Application.java
@@ -3,19 +3,11 @@ package br.com.alura.listavip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
 public class Application {
 	
-	@RequestMapping("/")
-	@ResponseBody
-	public String ola() {
-		return "Olá, bem vindo ao sistema lista vip";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -0,0 +1,13 @@
+package br.com.alura.listavip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConvidadoController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
