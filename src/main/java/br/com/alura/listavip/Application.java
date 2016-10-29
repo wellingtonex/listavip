adec120ec6c82b9520fef94b80a0734244cc8934
wellington.siqueira <wellingtonex@gmail.com>
2016-10-29 16:06:14 -0300

    movendo configuração de banco de dados e da porta para application.properties

--- src/main/java/br/com/alura/listavip/Application.java
+++ src/main/java/br/com/alura/listavip/Application.java
@@ -1,11 +1,7 @@
 package br.com.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Application {
@@ -13,17 +9,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Bean
-	public DataSource dataSource(){
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");		
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		return dataSource;
-	}
-	
-	
-
 }
